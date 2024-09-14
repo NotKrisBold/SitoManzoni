0998762868206bbe54f54ec8b484463a8c4bc7fe
KrisBold12 <kristianboldini@gmail.com>
2024-09-14 18:47:12 +0200

    sistemata home

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -5,8 +5,40 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  scrollTo(sectionId: string): void {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      // Calcola l'altezza della navbar
+      const navbarHeight = document.querySelector('.custom-navbar')?.clientHeight || 0;
+      // Calcola la posizione di scroll
+      const elementPosition = element.getBoundingClientRect().top + window.pageYOffset;
+      const offsetPosition = elementPosition - navbarHeight;
 
+      this.smoothScrollTo(offsetPosition, 2000); // Passa la durata in millisecondi
+    }
+  }
+
+  private smoothScrollTo(targetPosition: number, duration: number): void {
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    let startTime: number | null = null;
+
+    const animateScroll = (currentTime: number) => {
+      if (startTime === null) startTime = currentTime;
+      const timeElapsed = currentTime - startTime;
+      const progress = Math.min(timeElapsed / duration, 1);
+      const easeInOutQuad = (t: number) => t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t; // Funzione di easing
+
+      window.scrollTo(0, startPosition + distance * easeInOutQuad(progress));
+
+      if (timeElapsed < duration) {
+        requestAnimationFrame(animateScroll);
+      }
+    };
+
+    requestAnimationFrame(animateScroll);
+  }
 }
